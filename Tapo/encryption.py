75f6ecfef3405b5a37d14607af24db1bd6f54ec6
Luca Fenu <luca.fenu01@gmail.com>
2023-12-14 23:48:34 +0100

    Added support for the new encryption Added a property which contains the Requests session, to avoid using a cookies variable

--- Tapo/encryption.py
+++ Tapo/encryption.py
@@ -1,8 +1,8 @@
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import AES
-from Crypto.Cipher import PKCS1_v1_5
-from Crypto.Hash import SHA1
+from Crypto.Cipher import AES, PKCS1_v1_5
+from Crypto.Hash import SHA1, SHA256
 from Crypto.Util import Padding
+from Crypto.Random import get_random_bytes
 import base64
 
 
@@ -12,11 +12,27 @@ def generateKeyPair() -> dict[str, str]:
   publicKey = key.publickey().export_key(pkcs=8)
 
   tapoKeyPair = {
-    "public": publicKey.decode('utf-8'),
-    "private": privateKey.decode('utf-8'),
+    "public": publicKey.decode('UTF-8'),
+    "private": privateKey.decode('UTF-8'),
   }
 
   return tapoKeyPair
+
+
+# Just wrap "get_random_bytes" to use the encryption module instead of the Crypto one
+def getRandomBytes(size=16):
+  return get_random_bytes(size)
+
+
+# Calculate the auth hash used in the new encryption
+# It is different from "calcSHA256" because you must calculate the SHA1 of the parameters
+def calcAuthHash(username: bytes, password: bytes) -> bytes:
+  return SHA256.new(SHA1.new(username).digest() + SHA1.new(password).digest()).digest()
+
+
+# Just wrap SHA256 to use the encryption module instead of the Crypto one
+def calcSHA256(text: bytes) -> bytes:
+  return SHA256.new(text).digest()
 
 
 def decodeTapoKey(tapoKey: str, tapoKeyPair: dict[str, str]) -> dict[str, bytes] | None:
@@ -42,14 +58,24 @@ def shaDigestEmail(email: str) -> str:
   return emailHash
 
 
-def encryptJsonData(decodedTapoKey, jsonData: str):
-  try:
-    aes = AES.new(decodedTapoKey["secretKeySpec"], AES.MODE_CBC, IV=decodedTapoKey["ivParameterSpec"])
-    padJsonData = Padding.pad(jsonData.encode('utf-8'), 16, 'pkcs7')
-    encryptedJsonData = aes.encrypt(padJsonData)
-    return base64.b64encode(encryptedJsonData).decode()
-  except Exception as e:
-    raise e
+def encryptJsonData(decodedTapoKey: dict, jsonData: str) -> str:
+    try:
+        aes = AES.new(decodedTapoKey["secretKeySpec"], AES.MODE_CBC, IV=decodedTapoKey["ivParameterSpec"])
+        padJsonData = Padding.pad(jsonData.encode('utf-8'), 16, 'pkcs7')
+        encryptedJsonData = aes.encrypt(padJsonData)
+        return base64.b64encode(encryptedJsonData).decode('UTF-8')
+    except Exception as e:
+        raise e
+
+
+def encryptJsonDataNew(decodedTapoKey: dict, jsonData: str, seq: int) -> bytes:
+    try:
+        padJsonData = Padding.pad(jsonData.encode('utf-8'), 16, 'pkcs7')
+        aes = AES.new(decodedTapoKey["secretKeySpec"], AES.MODE_CBC, IV=decodedTapoKey["ivParameterSpec"] + seq.to_bytes(4, "big", signed=True))
+        encryptedJsonData = aes.encrypt(padJsonData)
+        return encryptedJsonData
+    except Exception as e:
+        raise e
 
 
 def decryptJsonData(decodedTapoKey, encryptedJsonData) -> str:
@@ -57,4 +83,13 @@ def decryptJsonData(decodedTapoKey, encryptedJsonData) -> str:
     aes = AES.new(decodedTapoKey["secretKeySpec"], AES.MODE_CBC, IV=decodedTapoKey["ivParameterSpec"])
 
     decryptedJsonData = aes.decrypt(encryptedJsonData)
-    return decryptedJsonData.decode().strip()
+    return decryptedJsonData.decode('UTF-8').strip()
+
+
+def decryptJsonDataNew(decodedTapoKey, encryptedJsonData, seq: int) -> str:
+    aes = AES.new(decodedTapoKey["secretKeySpec"], AES.MODE_CBC, IV=decodedTapoKey["ivParameterSpec"] + seq.to_bytes(4, "big", signed=True))
+
+    decryptedJsonData = aes.decrypt(encryptedJsonData)
+    decryptedJsonData = Padding.unpad(decryptedJsonData, 16, 'pkcs7')
+
+    return decryptedJsonData.decode('UTF-8').strip()
--- Tapo/tapo.py
+++ Tapo/tapo.py
@@ -3,7 +3,6 @@ import Tapo.encryption as encryption
 import requests
 import json
 from base64 import b64encode
-
 import logging
 
 
@@ -11,12 +10,22 @@ class Tapo:
     def __init__(self, IP: str, username: str, password: str, terminalUUID = '00-00-00-00-00-00'):
         # Create a logger to use in the module
         self.logger = logging.getLogger('Tapo/API')
+        self.session = requests.Session()
 
         self.IP = IP
         self.user = username
         self.password = password
-        self.keyPair = encryption.generateKeyPair()
         self.terminalUUID = terminalUUID
+
+        if self.__init_old__() is None:
+            if self.__init_new__() is None:
+                return None
+        
+        self.logger.info('Login succesful!')
+
+
+    def __init_old__(self):
+        self.keyPair = encryption.generateKeyPair()
 
         # Get the keys and log in.
         # If there was an error during either of the steps, the returned object will just be "None"
@@ -25,7 +34,39 @@ class Tapo:
         
         if not self.__login__():
             return None
-    
+
+
+    # There is now a new protocol used to "secure" queries to the plugs
+    def __init_new__(self):
+        # Generate random bytes and start the authentication to the plug
+        localSeed = encryption.getRandomBytes(16)
+        response = self.session.post("http://{}/app".format(self.IP) + '/handshake1', data=localSeed, verify=False).content
+
+        # Get the server hashes, generate the auth hash and compare it to the one on the server
+        remoteSeed, serverHash = response[0:16], response[16:]
+
+        authHash = encryption.calcAuthHash(self.user.encode('utf-8'), self.password.encode('utf-8'))
+        localSeedAuthHash = encryption.calcSHA256(localSeed + remoteSeed + authHash)
+
+        if serverHash != localSeedAuthHash:
+            logging.error('Wrong login!\nServer Hash: ' + serverHash + '\nLocal Auth: ' + localSeedAuthHash)
+            return None
+
+        # Continue the handshake by sending a new hash generated from the hashes above
+        response = self.session.post("http://{}/app".format(self.IP) + '/handshake2', data=encryption.calcSHA256(remoteSeed + localSeed + authHash), verify=False)
+
+        # Generate the AES key/IV by using the hashes
+        ivSeq = encryption.calcSHA256(b"iv" + localSeed + remoteSeed + authHash)
+        self.decodedKey = {
+            "secretKeySpec": encryption.calcSHA256(b"lsk" + localSeed + remoteSeed + authHash)[:16],
+            "ivParameterSpec": ivSeq[:12],
+        }
+
+        # Get the sequence number and generate the signature that will be used for every request
+        self.seq = int.from_bytes(ivSeq[-4:], "big", signed=True)
+        self.signature = encryption.calcSHA256(b"ldk" + localSeed + remoteSeed + authHash)[:28]
+
+
     # Get the keys used to encrypt/decrypt the requests directed to the plug.
     # @return bool Whether the operation was succesful or not
     def __get_keys__(self) -> bool:
@@ -37,19 +78,13 @@ class Tapo:
             "requestTimeMils":0
         }
 
-        response = requests.post("http://{}/app".format(self.IP), data=json.dumps(data), verify=False)
+        response = self.session.post("http://{}/app".format(self.IP), data=json.dumps(data), verify=False)
 
         if response.status_code != 200 or b'error' in response.content:
-            self.logger.fatal('Error during key handshake!\n' + response.content.decode('utf-8'))
+            self.logger.fatal('Old login failed!')
             return False
 
         self.key = json.loads(response.content.decode('utf-8'))['result']['key']
-
-        # Store the cookie in the object, because it will be used during every request.
-        cookie = response.headers["Set-Cookie"].split(';')[0].split('=')
-        self.cookies = {
-            cookie[0]: cookie[1]
-        }
 
         self.decodedKey = encryption.decodeTapoKey(self.key, self.keyPair)
 
@@ -80,7 +115,7 @@ class Tapo:
         }
 
 
-        response = requests.post("http://{}/app".format(self.IP), cookies=self.cookies, data=json.dumps(secureData), verify=False)
+        response = self.session.post("http://{}/app".format(self.IP), data=json.dumps(secureData), verify=False)
 
         if response.status_code != 200 or b'error' in response.content:
             self.logger.fatal('Error during login request!\n' + response.content.decode('utf-8'))
@@ -95,24 +130,48 @@ class Tapo:
 
 
     def __request__(self, data: dict) -> str:
-        encyptedJsonData = encryption.encryptJsonData(self.decodedKey, json.dumps(data))
-        secureData = {
-            "method":"securePassthrough",
-            "params":{
-            "request": encyptedJsonData
+        # Pick the encryption method depending on whether the seq var exists
+        if self.seq:
+            # Increase seq number before than sending the request
+            self.seq += 1
+            encryptedJsonData = encryption.encryptJsonDataNew(self.decodedKey, json.dumps(data), self.seq)
+
+            sig = encryption.calcSHA256(self.signature + self.seq.to_bytes(4, "big", signed=True) + encryptedJsonData)
+
+            encryptedJsonData = sig + encryptedJsonData
+
+            url = "http://{}/app/request".format(self.IP)
+            response = self.session.post(url, data=encryptedJsonData, params={"seq": self.seq})
+
+            if response.status_code != 200 or b'error' in response.content:
+                self.logger.fatal('Error during request!\nRequest: ' + json.dumps(data) + '\nResponse:' + response.content.decode('utf-8'))
+                return None
+
+            decryptedJsonData = encryption.decryptJsonDataNew(self.decodedKey, response.content[32:], self.seq)
+
+            return decryptedJsonData
+        else:
+            encryptedJsonData = encryption.encryptJsonData(self.decodedKey, json.dumps(data))
+
+            secureData = {
+                "method":"securePassthrough",
+                "params": {
+                "request": encryptedJsonData
+                }
             }
-        }
 
-        response = requests.post("http://{}/app?token={}".format(self.IP,self.authToken), cookies=self.cookies, data=json.dumps(secureData), verify=False)
-        
-        if response.status_code != 200 or b'error' in response.content:
-            self.logger.fatal('Error during request!\nRequest: ' + json.dumps(data) + '\nResponse:' + response.content.decode('utf-8'))
-            return None
+            url = "http://{}/app?token={}".format(self.IP,self.authToken)
 
-        encryptedJsonResponse = json.loads(response.content.decode("utf-8"))['result']['response']
-        
-        decryptedJsonData = encryption.decryptJsonData(self.decodedKey, encryptedJsonResponse)
-        return "".join(n for n in decryptedJsonData if ord(n) >= 32 and ord(n) <= 126)
+            response = self.session.post(url, data=json.dumps(secureData), verify=False)
+
+            if response.status_code != 200 or b'error' in response.content:
+                self.logger.fatal('Error during request!\nRequest: ' + json.dumps(data) + '\nResponse:' + response.content.decode('utf-8'))
+                return None
+
+            encryptedJsonResponse = json.loads(response.content.decode("utf-8"))['result']['response']
+            decryptedJsonData = encryption.decryptJsonData(self.decodedKey, encryptedJsonResponse)
+
+            return "".join(n for n in decryptedJsonData if ord(n) >= 32 and ord(n) <= 126)
 
 
     def getDeviceInfo(self) -> dict:
