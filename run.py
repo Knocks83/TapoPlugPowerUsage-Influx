6580630598f04df32054ec3eb00cbbaf7c56d5f7
Luca Fenu <luca.fenu01@gmail.com>
2024-01-29 21:26:45 +0100

    Add check for unreachable plug

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 from datetime import datetime, timezone
 
+from requests.exceptions import ConnectTimeout
 import logging
 from time import sleep
 
@@ -12,13 +13,14 @@ logging.basicConfig(level=logging.INFO)
 logging.getLogger('urllib3').setLevel(logging.WARNING)
 logger = logging.getLogger('Tapo/Run')
 
-tapo = Tapo(cfg.tapoIP, cfg.tapoUser, cfg.tapoPwd, cfg.tapoTerminalUUID)
 
 influx = InfluxDBClient(url=cfg.influxURL, token=cfg.influxToken, org=cfg.influxOrg)
 write_api = influx.write_api(write_options=SYNCHRONOUS)
 
 
 try:
+    tapo = Tapo(cfg.tapoIP, cfg.tapoUser, cfg.tapoPwd, cfg.tapoTerminalUUID)
+
     while True:
         powerUsage = tapo.getCurrentPower()['result']['current_power']
         point = [{
@@ -37,6 +39,8 @@ try:
         sleep(cfg.loopDelay)
 except KeyboardInterrupt:
     logger.info('Caught KeyboardInterrupt')
+except ConnectTimeout:
+    logger.warning('Tapo plug unreachable')
 except Exception as e:
     logger.exception(e)
 finally:
